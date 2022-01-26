123ef323405692c7546e114b1dea9cdc06fb962e
silver2312 <phule23121999@gmail.com>
2022-01-26 12:32:02 +0700

    fix composer

--- resources/views/mod/truyen.blade.php
+++ resources/views/mod/truyen.blade.php
@@ -11,7 +11,7 @@ use App\Models\Truyen\TruyenSub;
 </style>
 <div class="col-12 mt-2">
     <!-- Search form -->
-    <form class="navbar-search navbar-search-light form-inline mr-sm-3 mb-2" id="navbar-search-main" method="GET" action="{{ url('mod/truyen/tim-kiem') }}" autocomplete="off">
+    <form class="navbar-search navbar-search-light form-inline mr-sm-3 mb-2 float-right" id="navbar-search-mod" method="GET" action="{{ url('mod/truyen/tim-kiem') }}" autocomplete="off">
         <div class="form-group mb-0">
             <div class="input-group input-group-alternative input-group-merge">
                 <div class="input-group-prepend">
@@ -20,22 +20,26 @@ use App\Models\Truyen\TruyenSub;
                 <input class="form-control" placeholder="Tên truyện,tác giả,người nhúng,.." type="search" name="tu_khoa">
             </div>
         </div>
-        <button type="button" class="close" data-action="search-close" data-target="#navbar-search-main" aria-label="Close">
+        <button type="button" class="close" data-action="search-close" data-target="#navbar-search-mod" aria-label="Close">
             <span aria-hidden="true">×</span>
         </button>
     </form>
+    <span class="d-sm-none">
+        <a class="nav-link" href="#!" data-action="search-show" data-target="#navbar-search-mod">
+            <i class="ni ni-zoom-split-in"></i>
+        </a>
+    </span>
     {{--  end Thêm chung_toc --}}
-    {{ $truyen->links('vendor.pagination.simple-default') }}
+    {{ $truyen->onEachSide(0)->links('vendor.pagination.simple-default') }}
     <div class="table-responsive">
         <table class="table table-flush table-dark">
             <thead class="thead-dark">
                 <tr>
                     <th scope="col">#</th>
+                    <th scope="col">Hình ảnh</th>
                     <th scope="col">Tiêu đề</th>
                     <th scope="col">Tác giả</th>
                     <th scope="col">Giới thiệu</th>
-                    <th scope="col">Giới thiệu</th>
-                    <th scope="col">Hình ảnh</th>
                     <th scope="col">Người nhúng(viết)</th>
                     <th scope="col">Chương - Thích - Không thích - Tủ - Quà</th>
                     <th scope="col">Thời gian</th>
@@ -64,20 +68,43 @@ use App\Models\Truyen\TruyenSub;
                     ?>
                     <tr>
                         <th scope="row">{{$value->id}}</th>
+                        <td><img src="{{$value->img}}" alt="{{$tieu_de}}" style="max-height: 70px;" onerror="this.src='https://i.imgur.com/hQRlkUR.png';" id="phongto"></td>
                         <td><a href="{{ url('truyen/'.$value->nguon.'/'.$value->id) }}">{{ $tieu_de }}</a> / {{$value->tieu_de}}</td>
                         <td>{{ $tac_gia }} / {{$value->tac_gia}}</td>
-                        <td>{{ $gioi_thieu }}</td>
-                        <td>{{$value->gioi_thieu}}</td>
-                        <td><img src="{{$value->img}}" alt="{{$tieu_de}}" style="max-height: 70px;" onerror="this.src='https://i.imgur.com/hQRlkUR.png';" id="phongto"></td>
+                        <td><a href="#!" data-toggle="modal" data-target="#gioi_thieu_{{$value->id}}" class="btn btn-sm btn-info">Giới thiệu</a></td>
                         <td><a href="{{ url('trang-ca-nhan/'.$value->nguoi_nhung) }}">{{check_name($value->nguoi_nhung)}}</a></td>
                         <td>{{$value->so_chuong}} - {{$value->tong_like}} - {{$value->dislike}} - {{$value->tu}} - {{$value->curr_gif}} /  {{$value->gift}}</td>
                         <td>{{$value->time_suf}} /  {{$value->time_up}}</td>
-                        <td><a href="{{url('mod/truyen/de-cu/'.$value->id)}}" class="btn btn-sm btn-primary" onclick="return confirm('Bạn có chắc muốn đề cử truyện này không ?')">Đề cử</a></td>
+                        <td>@if($value->de_cu == 0)<a href="{{url('mod/truyen/de-cu/'.$value->id)}}" class="btn btn-sm btn-primary" onclick="return confirm('Bạn có chắc muốn đề cử truyện này không ?')">Đề cử</a>@else Truyện đang được đề cử @endif</td>
                     </tr>
+                    <!-- Modal -->
+                    <div class="modal fade" id="gioi_thieu_{{$value->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Giới thiệu</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="table-responsive">
+                                    <div class="row">
+                                        <div class="col-6">{{ $value->gioi_thieu }}</div>
+                                        <div class="col-6">{{ $gioi_thieu }}</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Đóng</button>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
                 @endforeach
             </tbody>
         </table>
     </div>
-    {{ $truyen->links('vendor.pagination.simple-default') }}
+    {{ $truyen->onEachSide(0)->links('vendor.pagination.simple-default') }}
 </div>
 @endsection
--- resources/views/pages/truyen.blade.php
+++ resources/views/pages/truyen.blade.php
@@ -11,17 +11,16 @@
 ?>
 <div class="container mt-5">
     <h2 class="text-center">Bạn đang có : {{$count}} truyện</h2>
-    {{ $truyen->links('vendor.pagination.simple-default') }}
+    {{ $truyen->onEachSide(0)->links('vendor.pagination.simple-default') }}
     <div class="table-responsive">
         <table class="table table-flush table-dark">
             <thead class="thead-dark">
                 <tr>
                     <th scope="col">#</th>
+                    <th scope="col">Ảnh truyện</th>
                     <th scope="col">Tiêu đề</th>
                     <th scope="col">Tác giả</th>
                     <th scope="col">Giới thiệu</th>
-                    <th scope="col">Giới thiệu</th>
-                    <th scope="col">Ảnh truyện</th>
                     <th scope="col">Nguồn</th>
                     <th scope="col">Thời gian</th>
                     <th scope="col">Chương - Thích - Không thích - Tủ - Quà</th>
@@ -49,20 +48,43 @@
                     ?>
                     <tr>
                         <th scope="row">{{ $key++ }}</th>
+                        <td><img src="{{$value->img}}" id="phongto" style="max-height:70px;" onerror="this.src='https://i.imgur.com/hQRlkUR.png';" ></td>
                         <td><a href="{{ url('truyen/'.$value->nguon.'/'.$value->id) }}">{{ $tieu_de }}</a> / {{$value->tieu_de}}</td>
                         <td>{{ $tac_gia }} / {{$value->tac_gia}}</td>
-                        <td>{{ $gioi_thieu }}</td>
-                        <td>{{$value->gioi_thieu}}</td>
-                        <td><img src="{{$value->img}}" id="phongto" style="max-height:70px;" onerror="this.src='https://i.imgur.com/hQRlkUR.png';" ></td>
+                        <td><a href="#!" data-toggle="modal" data-target="#gioi_thieu_{{$value->id}}" class="btn btn-sm btn-info">Giới thiệu</a></td>
                         <td>{{$value->nguon}}</td>
                         <td>{{$value->time_suf}} - {{$value->time_up}}</td>
                         <td>{{$value->so_chuong}} - {{$value->tong_like}} - {{$value->dislike}} - {{$value->tu}} - {{$value->curr_gif}} /  {{$value->gift}}</td>
                     </tr>
+                    <!-- Modal -->
+                    <div class="modal fade" id="gioi_thieu_{{$value->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Giới thiệu</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="table-responsive">
+                                    <div class="row">
+                                        <div class="col-6">{{ $value->gioi_thieu }}</div>
+                                        <div class="col-6">{{ $gioi_thieu }}</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Đóng</button>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
                 @endforeach
             </tbody>
         </table>
     </div>
-    {{ $truyen->links('vendor.pagination.simple-default') }}
+    {{ $truyen->onEachSide(0)->links('vendor.pagination.simple-default') }}
 </div>
 
 @endsection
